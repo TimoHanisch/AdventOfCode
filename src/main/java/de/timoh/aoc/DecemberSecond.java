b6c5df3d9ad07875c3a0345c8de3bb1845ceeac5
Timo Hanisch <timohanisch@googlemail.com>
2015-12-02 13:54:43 +0100

    Small change for summing on dec 2.

--- src/main/java/de/timoh/aoc/DecemberSecond.java
+++ src/main/java/de/timoh/aoc/DecemberSecond.java
@@ -25,7 +25,7 @@ public class DecemberSecond {
                         .map(Integer::parseInt).toArray())
                 .map(arr -> Arrays.stream(new int[]{(int) arr[0] * (int) arr[1], (int) arr[1] * (int) arr[2], (int) arr[2] * (int) arr[0]}).sorted().toArray())
                 .map(arr -> arr[0] + arr[0] * 2 + arr[1] * 2 + arr[2] * 2)
-                .reduce(0, (sum, v) -> sum + v, (s1, s2) -> s1 + s2);
+                .reduce(0, Integer::sum);
         System.out.println(value);
     }
 
@@ -34,7 +34,7 @@ public class DecemberSecond {
                 .map(s -> Arrays.stream(s.split("x"))
                         .map(Integer::parseInt).sorted().toArray())
                 .map(arr -> (int) arr[0] * 2 + (int) arr[1] * 2 + (int) arr[0] * (int) arr[1] * (int) arr[2])
-                .reduce(0, (sum, v) -> sum + v, (s1, s2) -> s1 + s2);
+                .reduce(0, Integer::sum);
         System.out.println(value);
     }
 }
