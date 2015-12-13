b0db1319eb89d648389cb6e68023375d0b36c1ed
Timo Hanisch <timo.hanisch@jacksonmobile.de>
2015-12-13 18:01:48 +0100

    Started thirteenth of dec

---
+++ src/main/java/de/timoh/aoc/DecemberThirteenth.java
@@ -0,0 +1,13 @@
+package de.timoh.aoc;
+
+/**
+ * DecemberThirteenth
+ *
+ * @author Timo Hanisch
+ * @since 13.12.2015
+ */
+public class DecemberThirteenth {
+    public static void main(String[] args) {
+
+    }
+}
